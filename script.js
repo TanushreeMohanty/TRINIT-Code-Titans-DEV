76109203bf8e6a202f9f97f3a8349c76ab41df03
TanushreeMohanty <122718553+TanushreeMohanty@users.noreply.github.com>
2024-03-09 10:44:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// NAVBAR
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('mainNav');
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-transparent');
+    } else {
+        navbar.classList.remove('navbar-transparent');
+    }
+});
+
+// LOGIN/SIGNUP
 document.getElementById('toggleSignup').addEventListener('click', function () {
     document.getElementById('loginForm').style.display = 'none';
     document.getElementById('signupForm').style.display = 'block';
